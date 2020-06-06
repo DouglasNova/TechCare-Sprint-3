4c8707f3b0d7e960552cb104eac9e41fd5e44db6
Guilherme-Alves-Ferreira <guilherme.ferreira@bandtec.com.br>
2020-06-06 17:45:28 -0300

    Atualizações na dashboard e página sobre

--- Banco de dados/medidas_node.sql
+++ Banco de dados/medidas_node.sql
@@ -1,14 +1,13 @@
-create database sensor;
-use sensor;
-
 create table medidas (
     id int primary key auto_increment,
-    temp varchar (25) NOT NULL,
+    temp varchar(25) NOT NULL,
     temp_value DOUBLE (10,2),
-    humd varchar (25) NOT NULL,
+    humd varchar(25) NOT NULL,
     humd_value DOUBLE (10,2)
 );
 
 select * from medidas;
+INSERT INTO medidas (temp,temp_value,humd,humd_value) VALUES ('Temperatura',?), ('Umidade',?);
+insert into medidas values (null, 'Temperatura', ?, 'Umidade', ?);
 
 
