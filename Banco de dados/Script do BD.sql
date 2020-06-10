cfe2fd0670eb9156ecb7f660dfae2e726eb43c8d
Guilherme-Alves-Ferreira <guilherme.ferreira@bandtec.com.br>
2020-06-10 15:59:42 -0300

    Subindo modificações no banco e arquivos da aula do yoshi

--- Banco de dados/Script do BD.sql
+++ Banco de dados/Script do BD.sql
@@ -1,33 +1,45 @@
 create database TechCare;
 use TechCare;
+
 Create table Hospital(
 idHospital int primary key auto_increment,
 Nome_Hospital varchar(45), 
 Rua varchar(45),
 Numero varchar(5), 
-CEP char(8), 
+CEP char(12), 
 Bairro varchar(45)
 );
 
+insert into Hospital values
+    (1, 'Albert Einstein', 'Av. Albert Einstein', '627', '05652-900', 'Jardim Leonor'),
+    (2, 'Sírio Libanês', 'Joaquim Floriano', '533', '04534-011', 'Itaim Bibi');
+
 Create table Ala(
-idAla int, 
+idAla int primary key auto_increment, 
 fkHospitalAla int, 
-foreign key (fkHospitalAla) references Hospital (idHospital), 
-primary key (idAla, fkHospitalAla)
+foreign key (fkHospitalAla) references Hospital (idHospital)
 );
 
+insert into Ala values
+    (null, 1),
+    (null, 2);
+
 create table Funcionario(
- Email_func int primary key auto_increment, 
- fkFuncPermissão int, 
- Senha varchar(18),
- fkHospitalAla int, -- Pensar em um nome para essa fk
- foreign key (fkHospitalAla) references Hospital (idHospital),
- foreign key (fkFuncPermissão) references Funcionario (Email_func), 
- Nome_Funcionario varchar (45),
- Permissão char(1)
- check(Permissão = 's' or Permissão = 'n'),
- CPF char(11)
+    ID_Funcio int primary key auto_increment,
+    Email_func varchar(45), 
+    Senha varchar(18),
+    fkHospitalFunc int,
+foreign key (fkHospitalFunc) 
+references Hospital (idHospital),
+    Nome_Funcionario varchar (45),
+    Permissão bit,
+ check(Permissão = 1 or Permissão = 0),
+    CPF char(11)
 );
+
+insert into Funcionario value
+    (null, 'fulano1@alberteinster.com', 123, 1, 'Julio Silveira', 1, 45187602802),
+    (null, 'fulano2@siriolibanes.com', 456, 2, 'Gabriel da Silva', 1, 68748934594);
 
 create table Sensor(
 idSensor int primary key auto_increment, 
@@ -42,7 +54,7 @@ foreign key (fkSensor) references Sensor (idSensor),
 primary key (idDados, fkSensor),
 Umidade Double (10,2), 
 Temperatura Double (10,2), 
-Data_dados date, 
-Hora time
+Data_hora datetime
 );
+
 
