8fe61c9fe934cfbaf11b8d40e53a6de129770435
unknown <douglas.nova@bandtec.com.br>
2020-06-11 17:32:43 -0300

    atualização

--- projeto-aquisicao-local/projeto-arduino/Node/node_database/node_database/NODE4 (1)/app/database.js
+++ projeto-aquisicao-local/projeto-arduino/Node/node_database/node_database/NODE4 (1)/app/database.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host     : 'localhost',
     port     : '3306',
-    user     : 'techcare',
+    user     : 'douglas',
     password : 'urubu100',
     database : 'sensor'
 });
