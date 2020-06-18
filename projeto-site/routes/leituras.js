98ddab85490d2230f6f6df1957a8add309372883
unknown <douglas.nova@bandtec.com.br>
2020-06-18 18:31:19 -0300

    seletor de alas e sensor

--- projeto-site/routes/leituras.js
+++ projeto-site/routes/leituras.js
@@ -3,14 +3,16 @@ var router = express.Router();
 var sequelize = require('../models').sequelize;
 var Leitura = require('../models').Leitura;
 
+
+
 /* Recuperar as últimas N leituras */
-router.get('/ultimas', function(req, res, next) {
-	
+router.get('/ultimas', function (req, res, next) {
+
 	// quantas são as últimas leituras que quer? 8 está bom?
 	const limite_linhas = 7;
 
 	console.log(`Recuperando as últimas ${limite_linhas} leituras`);
-	
+
 	const instrucaoSql = `select top ${limite_linhas} 
 						Umidade, 
 						Temperatura, 
@@ -20,21 +22,21 @@ router.get('/ultimas', function(req, res, next) {
 
 	sequelize.query(instrucaoSql, {
 		model: Leitura,
-		mapToModel: true 
-	  })
-	  .then(resultado => {
+		mapToModel: true
+	})
+		.then(resultado => {
 			console.log(`Encontrados: ${resultado.length}`);
 			res.json(resultado);
-	  }).catch(erro => {
+		}).catch(erro => {
 			console.error(erro);
 			res.status(500).send(erro.message);
-	  });
+		});
 });
 
 
 // tempo real (último valor de cada leitura)
 router.get('/tempo-real', function (req, res, next) {
-	
+
 	console.log(`Recuperando a última leitura`);
 
 	const instrucaoSql = `select top 1 Umidade, Temperatura, FORMAT(Data_hora,'HH:mm:ss') as momento_grafico  
@@ -42,18 +44,19 @@ router.get('/tempo-real', function (req, res, next) {
 
 	sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
 		.then(resultado => {
+			console.log(resultado)
 			res.json(resultado[0]);
 		}).catch(erro => {
 			console.error(erro);
 			res.status(500).send(erro.message);
 		});
-  
+
 });
 
 
 // estatísticas (max, min, média, mediana, quartis etc)
 router.get('/estatisticas', function (req, res, next) {
-	
+
 	console.log(`Recuperando as estatísticas atuais`);
 
 	const instrucaoSql = `select 
@@ -72,7 +75,61 @@ router.get('/estatisticas', function (req, res, next) {
 			console.error(erro);
 			res.status(500).send(erro.message);
 		});
-  
+
+});
+
+router.get('/ala', function (req, res, next) {
+
+
+	const instrucaoSql = `select idAla  
+						from Ala `;
+
+	sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
+		.then(resultado => {
+			console.log(resultado)
+			res.json(resultado);
+		}).catch(erro => {
+			console.error(erro);
+			res.status(500).send(erro.message);
+		});
+
+});
+router.get('/sensor', function (req, res, next) {
+
+
+	const instrucaoSql = `select *  
+						from sensor `;
+
+	sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
+		.then(resultado => {
+			console.log(resultado)
+			res.json(resultado);
+		}).catch(erro => {
+			console.error(erro);
+			res.status(500).send(erro.message);
+		});
+
+});
+router.get('/dadosSensor', function (req, res, next) {
+
+
+	const instrucaoSql = `select  
+						Umidade, 
+						Temperatura, 
+						Data_hora,
+						FORMAT(Data_hora,'HH:mm:ss') as momento_grafico 
+						from Dados_do_sensor order by ID_dados_rows desc`;
+
+
+	sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
+		.then(resultado => {
+			console.log(resultado)
+			res.json(resultado);
+		}).catch(erro => {
+			console.error(erro);
+			res.status(500).send(erro.message);
+		});
+
 });
 
 
