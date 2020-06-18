2dec83001fc3a542de2395a52c796d14b0cf0ad2
Guilherme-Alves-Ferreira <guilherme.ferreira@bandtec.com.br>
2020-06-18 15:59:45 -0300

    Subindo atualizações da dashboard

--- projeto-aquisicao-local/projeto-arduino/Node/node_database/NODE4 (1)/app/database.js
+++ projeto-aquisicao-local/projeto-arduino/Node/node_database/NODE4 (1)/app/database.js
@@ -1,17 +1,21 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    port     : '3306',
-    user     : 'techcare',
-    password : 'urubu100',
-    database : 'TechCare'
-});
-
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log('Conectado com sucesso!')
-});
-
-
-
-module.exports = connection;
+var Connection = require('tedious').Connection;  
+    var config = {  
+        server: 'servertechcare11a.database.windows.net',  //update me
+        authentication: {
+            type: 'default',
+            options: {
+                userName: 'adminlocal', //update me
+                password: '#Gftechcare'  //update me
+            }
+        },
+        options: {
+            // If you are on Microsoft Azure, you need encryption:
+            encrypt: true,
+            database: 'bdTechCare'  //update me
+        }
+    };  
+    var connection = new Connection(config);  
+    connection.on('connect', function(err) {  
+        // If no error, then good to proceed.
+        console.log("Connected");  
+    });
